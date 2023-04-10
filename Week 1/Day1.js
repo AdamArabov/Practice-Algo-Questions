8f1cffc644b69c3ea3701cd8339e6ae52dc82832
AdamArabov <arabovadam@yahoo.com>
2023-04-10 10:03:12 -0400

    Binary tree Path Solved

---
+++ Week 1/Day1.js
@@ -0,0 +1,29 @@
+//Binary tree paths//
+
+let binarysearchtree = function(root){
+    if(!root){
+        return [];
+    }
+
+    const result = [];
+
+    function findPath(node,currentPath){
+        currentPath += node.val;
+        if(!node.left && !node.right){
+            result.push(currentPath)
+        }
+
+        if(node.left){
+            findPath(node.left, currentPath + '->')
+        }
+
+        if(node.right){
+            findPath(node.right, currentPath + '->')
+        }
+    }
+
+    findPath(root, '');
+    return result 
+};
+
+console.log(binarysearchtree)
