f0fbc454809dcba2235c53454f6867487e043e7e
AdamArabov <arabovadam@yahoo.com>
2023-04-11 10:52:08 -0400

    Completed Day2

--- Week 1/Day2.js
+++ Week 1/Day2.js
@@ -48,3 +48,27 @@ var findMedianSortedArrays = function(nums1, nums2) {
   
   return median;
 };
+
+// Longest Palindromic Substring //
+
+var longestPalindrome = function(s) {
+    if(s.length<2) return s;
+    let start=0, maxLength=1;
+    function expandAroundCenter(left,right){
+        while (s[left] === s[right] && left >=0 && right < s.length)
+        {
+            if(right-left +1 > maxLength){
+                start =left;
+                maxLength = right - left +1;
+            }
+            left -=1;
+            right +=1;
+            
+        }
+    }
+    for (let i=0;i<s.length; i+=1){
+        expandAroundCenter(i-1,i+1);
+        expandAroundCenter(i, i+1)
+    }
+    return s.substring(start, start+maxLength)
+};
