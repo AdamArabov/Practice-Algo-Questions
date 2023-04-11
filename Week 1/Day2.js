847cc3dc44108ac006b24e375c5e95fbffb1b54a
AdamArabov <arabovadam@yahoo.com>
2023-04-11 10:37:59 -0400

    Day2 problem 2

--- Week 1/Day2.js
+++ Week 1/Day2.js
@@ -17,3 +17,34 @@ var lengthOfLongestSubstring = function(s) {
     }
     return max 
 };
+
+// Median of Two Sorted Arrays // 
+
+
+var findMedianSortedArrays = function(nums1, nums2) {
+    let num=[...nums1,...nums2]
+  num.sort(function(a, b){return a-b});
+  
+  let median;
+  
+ 
+
+
+  
+  let length=num.length;
+ 
+  
+      let mid = Math.floor(length/2);
+      if(length%2==1){
+          median=num[mid];
+          }
+  else{
+          median= (num[mid]+num[mid-1])/2.0;
+      }
+     
+          
+      
+
+  
+  return median;
+};
