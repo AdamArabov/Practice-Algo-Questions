f7f0e9bf703b06a942e4bb3dc94ef8990f055a3a
AdamArabov <arabovadam@yahoo.com>
2023-04-12 12:34:57 -0400

    question1 solved

--- Week 1/Day3.js
+++ Week 1/Day3.js
@@ -1,1 +1,24 @@
 // Zigzag Conversion//
+var convert = function(s, numRows) {
+    if (numRows == 1) return s
+
+    let n =s.length
+    let charsInSection = numRows * 2 - 2
+    let res = ''
+
+    for (let row = 0; row < numRows; row++) {
+        let i = row
+        while (i < n) {
+            res += s[i]
+
+            if (row != 0 && row != numRows-1) {
+                let charsInBetween = charsInSection - 2 * row
+                let secondIndex = i + charsInBetween
+                if (secondIndex < n) res += s[secondIndex]
+            }
+            i += charsInSection
+        }
+    }
+    return res
+};
+
