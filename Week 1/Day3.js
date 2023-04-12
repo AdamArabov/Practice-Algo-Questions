0ebef3bd1128b11c118cac5c0d55fa5ebf79f553
AdamArabov <arabovadam@yahoo.com>
2023-04-12 12:41:04 -0400

    question2 solved

--- Week 1/Day3.js
+++ Week 1/Day3.js
@@ -23,4 +23,15 @@ var convert = function(s, numRows) {
 };
 
 //Reverse Integer//
+var reverse = function(x) {
+    const sign = Math.sign(x);
+    let result =0;
+    while(x!==0){
+        result = result*10+x%10;
+        x= sign>0? Math.floor(x/10): Math.ceil(x/10);
+        }
 
+        const limit = Math.pow(2,31);
+        if(result<=-limit || result> limit) return 0;
+        return result;
+};
