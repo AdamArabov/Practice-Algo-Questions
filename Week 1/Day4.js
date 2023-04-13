e65012c8bebd88b32bd53e91fc6336d94d49af3b
AdamArabov <arabovadam@yahoo.com>
2023-04-13 15:11:12 -0400

    q1 solved

---
+++ Week 1/Day4.js
@@ -0,0 +1,10 @@
+var isPalindrome = function(x) {
+    let reverseString= ''
+    let xString = x.toString()
+
+    for(let i of xString){
+        reverseString = i + reverseString
+    }
+
+    return(reverseString === xString)
+};
