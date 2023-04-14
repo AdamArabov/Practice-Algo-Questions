c662ba845945c48eb234f59f5c60277f698497e4
AdamArabov <arabovadam@yahoo.com>
2023-04-14 13:36:11 -0400

    q6solved

--- Week 1/Day5.js
+++ Week 1/Day5.js
@@ -189,3 +189,50 @@ var threeSum = function(nums) {
   
     return result
 };
+
+//4Sum//
+var fourSum = function(nums, target) {
+    const result = []
+    
+    if (nums.length < 4) return result
+    
+    // O(logN)
+    nums.sort((a,b) => a - b)
+    
+    // O(n^3)
+    for (let i = 0; i < nums.length - 3; i++) {
+      // skip the same numbers
+      if (nums[i] === nums[i - 1]) continue
+      
+      for (let j = i + 1; j < nums.length - 2; j++) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) continue
+        
+        let k = j + 1
+        let l = nums.length - 1
+        
+        while (k < l) {
+          const sum = nums[i] + nums[j] + nums[k] + nums[l]
+          
+          if (sum === target) {
+            result.push([nums[i], nums[j], nums[k], nums[l]])
+          }
+          
+          if (sum <= target) {
+            k += 1
+            while (nums[k] === nums[k - 1]) {
+              k += 1
+            }
+          }
+          
+          if (sum >= target) {
+            l -= 1
+            while (nums[l] === nums[l + 1]) {
+              l -= 1
+            }
+          }
+        }
+      }
+    }
+  
+    return result
+}
