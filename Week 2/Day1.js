ea48756fefc6829eff32c945c60da086be5ea666
AdamArabov <arabovadam@yahoo.com>
2023-04-17 15:21:33 -0400

    q6solved

--- Week 2/Day1.js
+++ Week 2/Day1.js
@@ -248,4 +248,25 @@ class Solution {
   }
 
   //Remove Duplicates from Sorted Array//
+  /**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// TIME: O(N)
+// Space: O(1)
+var removeDuplicates = function(nums) {
+    let p1 = 0
+    let p2 = 0
+    
+    while (p2 < nums.length) {
+      if (nums[p2] !== nums[p2 - 1]) {
+        nums[p1] = nums[p2]
+        p1 += 1
+      }
+      
+      p2 += 1
+    }
   
+    return p1
+};
