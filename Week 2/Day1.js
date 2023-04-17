8bea9a5b9a761c69bac7b0e0043411eba7dbaa03
AdamArabov <arabovadam@yahoo.com>
2023-04-17 15:16:37 -0400

    q4solved

--- Week 2/Day1.js
+++ Week 2/Day1.js
@@ -182,3 +182,34 @@ var mergeKLists = function(lists) {
 
 //Swap Nodes in Pairs//
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    // reverse the nodes after head
+    const swap = (node) => {
+      if (node && node.next && node.next.next) {
+          const first = node.next
+          const second = node.next.next
+          first.next = second.next
+          second.next = first
+          node.next = second
+        
+          swap(first)
+      }
+    }
+    
+    const start = new ListNode(null)
+    start.next = head
+    swap(start)
+  
+    return start.next
+};
