9cb6f0f4f76115cfca6fa0085874564845a40157
AdamArabov <arabovadam@yahoo.com>
2023-04-18 21:52:02 -0400

    q1solved

--- Week 2/Day2.js
+++ Week 2/Day2.js
@@ -1,1 +1,21 @@
 //Remove Element//
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let p1 = 0
+    let p2 = 0
+    
+    while (p2 < nums.length) {
+      if (nums[p2] !== val) {
+        nums[p1] = nums[p2]
+        p1 += 1
+      }
+      
+      p2 += 1
+    }
+  
+    return p1
+};
