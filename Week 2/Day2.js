7e627f3142c67511f5ace4d9a458bf292fd54808
AdamArabov <arabovadam@yahoo.com>
2023-04-18 21:54:34 -0400

    q2solved

--- Week 2/Day2.js
+++ Week 2/Day2.js
@@ -21,3 +21,46 @@ var removeElement = function(nums, val) {
 };
 
 //Find the Index of the First Occurrence in a String//
+var strStr = function(haystack, needle) {
+    if (needle === '') return 0
+    
+    const jumpStep = [0, 0]
+    
+    let l = 1
+    let k = 0
+    // O(m)
+    // ababcaabc
+    //  ababcaabc
+    while (l < needle.length) {
+      if (needle[l] === needle[k]) {
+        l += 1
+        k += 1
+        jumpStep[l] = k
+      } else if (k === 0) {
+        l += 1
+        jumpStep[l] = 0
+      } else {
+        k = jumpStep[k]
+      }
+    }
+    
+    let i = 0
+    let j = 0
+    
+    // O(n)
+    while (i < haystack.length && j < needle.length) {
+      if (haystack[i] === needle[j]) {
+        i += 1
+        j += 1
+        if (j === needle.length) {
+          return i - needle.length
+        }
+      } else if (j > 0) {
+        j = jumpStep[j]
+      } else {
+        i += 1
+      }
+    }
+  
+    return -1
+};
