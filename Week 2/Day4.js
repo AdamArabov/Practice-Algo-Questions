47aa3fb0ae14325c6aea5cdfbc1a172cd09b2abf
AdamArabov <arabovadam@yahoo.com>
2023-04-24 13:34:37 -0400

    day4

--- Week 2/Day4.js
+++ Week 2/Day4.js
@@ -52,3 +52,22 @@ class Solution {
   }
 
 //   Search Insert Position ///
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+      int l = 0;
+      int r = nums.length;
+  
+      while (l < r) {
+        final int m = (l + r) / 2;
+        if (nums[m] == target)
+          return m;
+        if (nums[m] < target)
+          l = m + 1;
+        else
+          r = m;
+      }
+  
+      return l;
+    }
+  }
